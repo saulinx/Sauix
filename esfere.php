857d022379143eca7578cc810a5a8d8a74f25e8c
saulinx <saulomontana42@gmail.com>
2025-06-27 16:52:28 -0400

    primeiroCommit

---
+++ esfere.php
@@ -0,0 +1,9 @@
+<?php
+define("PI", 3.141592);
+$raio = 5;
+$area = 4*PI*pow($raio, 2);
+$volume = (4/3) * PI * pow($raio, 3);
+echo "Raio: $raio <br>";
+echo "Área: $area <br>";
+echo "Volume: $volume <br>";
+?>
